1645be8b25d44377b1f7b3e1ceb8309e3883a76c
Linus Mutugi <linusmutugi5178@gmail.com>
2020-02-28 16:29:55 +0300

    click event complete, order checkout modification complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,68 @@
 
 $(document).ready(function()
 {
+$("#regular").click(function()
+{
+  alert("regular");
+});
+$("#medium").click(function()
+{
+  alert("medium");
+});
+$("#large").click(function()
+{
+  alert("large");
+});
+$("#mega").click(function()
+{
+  alert("mega");
+});
+
+$("#Pepperoni").click(function()
+{
+  alert("Pepperoni");
+});
+$("#Mushrooms").click(function()
+{
+  alert("Mushrooms");
+});
+$("#Onions").click(function()
+{
+  alert("Onions");
+});
+$("#Sausage").click(function()
+{
+  alert("Sausage");
+});
+$("#Bacon").click(function()
+{
+  alert("Bacon");
+});
+
+$("#cheese").click(function()
+{
+  alert("Extra cheese");
+});
+$("#olives").click(function()
+{
+  alert("Black olives");
+});
+$("#peppers").click(function()
+{
+  alert("Green peppers");
+});
+$("#Pineapple").click(function()
+{
+  alert("Pineapple");
+});
+$("#Spinach").click(function()
+{
+  alert("Spinach");
+});
+
   $("button").click(function()
 {
   alert("your submission was successful");
 });
+
 });
